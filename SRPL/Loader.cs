b40bed1d2e26d0f3842bcf980371c594c0332d32
Dylan Renwick <skidsirongenesisdev@gmail.com>
2019-01-27 23:38:49 -0500

    Added SRPL, main project for plugin loader

---
+++ SRPL/Loader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SRPL
+{
+    public static class Loader
+    {
+        /// <summary>
+        /// Initializes the plugin loader. Called from SECTR_AudioSystem.OnEnable
+        /// </summary>
+        public static void Init()
+        {
+            // TODO: Initialize loader
+        }
+    }
+}
