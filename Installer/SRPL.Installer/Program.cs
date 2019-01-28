74ffbb6e092855af34820038d05abae332ca4eac
Dylan Renwick <skidsirongenesisdev@gmail.com>
2019-01-27 21:18:44 -0500

    Added util functions for filepaths

--- Installer/SRPL.Installer/Program.cs
+++ Installer/SRPL.Installer/Program.cs
@@ -1,14 +1,61 @@
 ﻿using System;
+using System.Reflection;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using Mono.Cecil;
 
 namespace SRPL.Installer
 {
     class Program
     {
-        static void Main(string[] args)
+        private static IAssemblyResolver asmResolver;
+
+        private static void Main(string[] args)
         {
+            asmResolver = new DefaultAssemblyResolver();
+        }
+
+        /// <summary>
+        /// Fetches the filepath of the Slime Rancher assembly, or prompts the user to provide it
+        /// </summary>
+        /// <returns>Absolute path of the Slime Rancher assembly</returns>
+        private static string getAssemblyFilePath()
+        {
+            // TODO: Add Autodetection logic
+            return @"D:\SteamLibrary\steamapps\common\Slime Rancher\SlimeRancher_Data\Managed\Assembly-Csharp.dll";
+        }
+
+        /// <summary>
+        /// Fetches the filepath of the DLL to be injected into the game
+        /// </summary>
+        /// <returns>Absolute path of the Loader assembl</returns>
+        private static string getLoaderFilePath()
+        {
+            // TODO: Don't assume loader DLL is in same directory as installer
+            return AppDomain.CurrentDomain.BaseDirectory + "\\SRPL.dll";
+        }
+
+        /// <summary>
+        /// Checks for access to the given file path. Will return true if the file exists, can be opened, and has Read/Write permissions
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns>Whether the application has sufficient access to the file</returns>
+        private static bool canOpenFile(string filePath)
+        {
+            try
+            {
+                using (FileStream stream = File.Open(filePath, FileMode.Open, FileAccess.ReadWrite))
+                {
+                    return true;
+                }
+            }
+            catch (IOException)
+            {
+                // TODO: Log exception
+                return false;
+            }
         }
     }
 }
