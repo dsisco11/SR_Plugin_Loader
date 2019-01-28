7fe79fc91557ee25d431c2dadc4e55bf8df9c0b6
Dylan Renwick <skidsirongenesisdev@gmail.com>
2019-01-28 14:00:54 -0500

    Added logging to DebugHud

--- SRPL/Debug/DebugHud.cs
+++ SRPL/Debug/DebugHud.cs
@@ -10,6 +10,7 @@ namespace SRPL.Debug
     {
         private static GameObject _hudRoot;
         private static DebugHudRenderer _hudRenderer;
+        private static List<string> lines = new List<string>();
 
         public static void Init()
         {
@@ -22,6 +23,28 @@ namespace SRPL.Debug
             {
                 _hudRenderer = _hudRoot.AddComponent<DebugHudRenderer>();
                 UnityEngine.Object.DontDestroyOnLoad(_hudRenderer);
+            }
+
+            Logging.Logger.onLog += Logger_onLog;
+        }
+
+        private static void Logger_onLog(Logging.LogLevel level, string module, string msg)
+        {
+            if (_hudRenderer == null)
+            {
+                lines.Add(msg);
+            }
+            else
+            {
+                if (lines.Count > 0)
+                {
+                    foreach (string s in lines)
+                    {
+                        _hudRenderer.AddLine(s);
+                    }
+                    lines.Clear();
+                }
+                _hudRenderer.AddLine(msg);
             }
         }
     }
